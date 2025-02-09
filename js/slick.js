8728764bea844ee2db469e7d6d961e3798269f78
olesyamaniuk <maniukolesya222@gmail.com>
2025-02-09 20:38:12 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -1,7 +1,8 @@
 
 
 $(function () {
-  $('.gallery__list').slick({
+
+    $('.gallery__list').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -9,11 +10,19 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.gallery-list-left',
     nextArrow: '.gallery-list-right',
+    customPaging: () => `<img src="/image/gallery/Component 4.png" alt="dot" class="custom-dot">`
+  });
+
+  $('.gallery__list').on('afterChange', (event, slick, currentSlide) => {
+    $('.gallery__list .slick-dots li').each((index, dot) => {
+      const imagePath = (index === currentSlide) ? '/image/gallery/Component 2.png' : '/image/gallery/Component 4.png';
+      $(dot).find('img').attr('src', imagePath);
+    });
   });
 });
 
@@ -27,7 +36,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: true,
+    dots: false,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.reviews-list-left',
